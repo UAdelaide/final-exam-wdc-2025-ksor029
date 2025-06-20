d1ec86a5fc2735c15d086a83363580abb8d6901a
ksor029 <unknown@student.adelaide.edu.au>
2025-06-20 14:46:22 +0000

    [automatic commit] 2025-06-20 14:46:22 UTC 49.186.67.227

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -29,6 +29,14 @@ router.get('/api/walkrequests/open', async function(req, res) {
 });
 
 /* GET Walker summary */
-
+router.get('/api/walkrequests/open', async function(req, res) {
+  const [rows] = await db.query(`
+    SELECT WalkRequests.request_id, Dogs.name AS dog_name, WalkRequests.requested_time, WalkRequests.duration_minutes, WalkRequests.location, Users.username FROM WalkRequests INNER JOIN Dogs
+      On WalkRequests.dog_id = Dogs.dog_id INNER JOIN Users
+      ON Dogs.owner_id = Users.user_id
+      WHERE WalkRequests.status = 'open';
+    `);
+  res.send(rows);
+});
 
 module.exports = router;
