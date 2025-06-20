d176622983002f792d442fe7cd735e1a6f981f53
ksor029 <unknown@student.adelaide.edu.au>
2025-06-20 12:56:30 +0000

    [automatic commit] 2025-06-20 12:56:30 UTC 49.181.211.143

--- part1/models/db.js
+++ part1/models/db.js
@@ -2,3 +2,7 @@ require('dotenv').config(); // load environmental variables
 
 const mysql = require('mysql2/promise');
 
+// create a pooled connection to handle concurrent queries efficiently
+const pool = mysql.createPool({
+   
+});
