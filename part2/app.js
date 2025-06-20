83a8811fb9b4b3767c846a7e483025f4d0d43a87
ksor029 <unknown@student.adelaide.edu.au>
2025-06-20 11:02:38 +0000

    [automatic commit] 2025-06-20 11:02:38 UTC 60.242.86.238

--- part2/app.js
+++ part2/app.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.use(function(req, res, next))
+app.use(function(req, res, next));
 
 
 // Routes
