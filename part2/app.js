1c315591f077e9f7b2131d20da53b9aef222be42
ksor029 <unknown@student.adelaide.edu.au>
2025-06-20 10:58:30 +0000

    [automatic commit] 2025-06-20 10:58:30 UTC 60.242.86.238

--- part2/app.js
+++ part2/app.js
@@ -10,7 +10,9 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 var mysql = require('mysql');
 
-var dbConnectionPool = mysql.crea
+var dbConnectionPool = mysql.createPool({
+    host: 'localhost'
+});
 
 
 // Routes
