780cf5f18e42883a948f436d0426426ab614ddde
ksor029 <unknown@student.adelaide.edu.au>
2025-06-20 11:24:22 +0000

    [automatic commit] 2025-06-20 11:24:22 UTC 60.242.86.238

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -17,15 +17,22 @@ router.get('/api/walkers/summary', function(req, res, next){
   // code
 });
 
-router.get('/result', function(req, res) {
-  // Connect to the database
-  req.pool.getConnection(function(err,connection) {
+router.get('/result', function(req, res, next) {
+  //Connect to the database
+  req.pool.getConnection( function(err,connection) {
     if (err) {
+    res.sendStatus(500);
+    return;
+    }
+    var query = "SHOW TABLES";
+    connection.query(query, function(err, rows, fields) {
+    connection.release(); // release connection
+      if (err) {
       res.sendStatus(500);
       return;
-    }
+      }
+      res.json(rows); //send response
+      });
   });
 });
-
 module.exports = router;
-  
