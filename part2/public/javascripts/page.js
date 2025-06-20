fdf65c54bd6ac6ad3d7737170a8c19f9f4947b55
ksor029 <unknown@student.adelaide.edu.au>
2025-06-20 20:26:22 +0000

    [automatic commit] 2025-06-20 20:26:22 UTC 49.186.67.227

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -188,7 +188,7 @@ function login(){
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             alert("Welcome "+this.responseText);
-            document.cookie = "username="+this.responseText; // Create cookie
+            document.cookie = "username="+this.responseText; // Create cookie. Q13
         } else if (this.readyState == 4 && this.status >= 400) {
             alert("Login failed");
         }
