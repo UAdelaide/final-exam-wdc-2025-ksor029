5e631e58ed6042cb25a88cb91baf1d3e2783ec43
ksor029 <unknown@student.adelaide.edu.au>
2025-06-20 13:27:41 +0000

    [automatic commit] 2025-06-20 13:27:40 UTC 49.181.211.143

--- part1/models/db.js
+++ part1/models/db.js
@@ -18,7 +18,7 @@ const pool = mysql.createPool({
    host: 'localhost',
    user: 'root',
    password: 'mypassword',
-   database: 'DogWalkService',
+   database: 'DogWalkService  ',
    waitForConnections: true,
    connectionLimit: 10,
    queueLimit: 0
