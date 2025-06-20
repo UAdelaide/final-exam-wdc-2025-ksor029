854d1383a8ee8ab7e9f5f0c6203763e71e4631cb
ksor029 <unknown@student.adelaide.edu.au>
2025-06-20 11:04:43 +0000

    [automatic commit] 2025-06-20 11:04:43 UTC 60.242.86.238

--- part2/app.js
+++ part2/app.js
@@ -25,8 +25,6 @@ app.use(function(req, res, next){
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
 
-const dogRoutes = require(./)
-
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
