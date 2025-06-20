7bde4080149ebe9902f34669ecea0cbbf887c80c
ksor029 <unknown@student.adelaide.edu.au>
2025-06-20 14:26:32 +0000

    [automatic commit] 2025-06-20 14:26:32 UTC ;; communications error to 208.67.222.222#53: timed out 49.186.67.227

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -118,6 +118,7 @@ FROM Dogs where name like 'Hades';
 SELECT
     Users.username AS walker_username
     (SELECT COUNT(*)
-    FROM W)
+    FROM WalkRatings
+    ON WalkRatings.walker_id = Users.username)
 FROM Users
 WHERE Users.role = 'walker';
