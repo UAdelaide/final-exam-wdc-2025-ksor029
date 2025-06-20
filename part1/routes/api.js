bb37a3af64a2bea2341bc1d912587d145ff26451
ksor029 <unknown@student.adelaide.edu.au>
2025-06-20 13:11:50 +0000

    [automatic commit] 2025-06-20 13:11:50 UTC 49.181.211.143

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -2,10 +2,3 @@ var express = require('express');
 var router = express.Router();
 const db = require('../models/db');
 
-/* GET Dogs */
-router.get('/dogs', async function(req, res) {
-  const [rows] = await db.query(
-    `SELECT * FROM Dogs`
-  );
-  res.send(rows);
-});
--- part1/routes/index.js
+++ part1/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+const db = require('../models/db');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+/* GET Dogs */
+router.get('/dogs', async function(req, res) {
+  const [rows] = await db.query(
+    `SELECT * FROM Dogs`
+  );
+  res.send(rows);
+});
+
+
 
 module.exports = router;
