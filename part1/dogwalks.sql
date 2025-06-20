55f47bfd0f5f3c42f20ee12c2d6d2cc57cad70ad
ksor029 <unknown@student.adelaide.edu.au>
2025-06-20 10:03:04 +0000

    [automatic commit] 2025-06-20 10:03:03 UTC 60.242.86.238

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -59,6 +59,6 @@ VALUES
 ('alice123', 'alice@example.com', 'hashed123', 'owner'),
 ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
 ('carol123', 'carol@example.com', 'hashed789', 'owner'),
-('timmy000', 'timbo@example.com', 'hashed789', 'owner'),
-('carol123', 'carol@example.com', 'hashed789', 'owner'),
+('timmy000', 'timbo@example.com', 'hashed159', 'walker'),
+('shadowcat', 'carol@example.com', 'hashed789', 'owner'),
 
