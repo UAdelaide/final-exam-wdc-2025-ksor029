737a29c7dc61eb446883603893d8d4f518efe52c
ksor029 <unknown@student.adelaide.edu.au>
2025-06-20 11:09:33 +0000

    [automatic commit] 2025-06-20 11:09:32 UTC 60.242.86.238

--- part2/app.js
+++ part2/app.js
@@ -2,35 +2,18 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config();
 
-var mysql = require('mysql');
-
-var dbConnectionPool = mysql.createPool({
-    host: 'localhost',
-    database: 'dogwalks'
-});
-
 const app = express();
 
 // Middleware
-app.use(function(req, res, next){
-    req.pool = dbConnectionPool;
-    next();
-});
-
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
 // Routes
+const walkRoutes = require('./routes/walkRoutes');
+const userRoutes = require('./routes/userRoutes');
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-
-// const walkRoutes = require('./routes/walkRoutes');
-// const userRoutes = require('./routes/userRoutes');
-
-// app.use('/api/walks', walkRoutes);
-// app.use('/api/users', userRoutes);
+app.use('/api/walks', walkRoutes);
+app.use('/api/users', userRoutes);
 
 // Export the app instead of listening here
 module.exports = app;
