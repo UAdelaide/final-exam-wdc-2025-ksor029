5de17fba04675040765a89dc739fb89fc6a5f1c8
ksor029 <unknown@student.adelaide.edu.au>
2025-06-20 10:01:30 +0000

    [automatic commit] 2025-06-20 10:01:30 UTC 60.242.86.238

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -56,4 +56,6 @@ CREATE TABLE WalkRatings (
 
 INSERT INTO Users (username, email, password_hash, role)
 VALUES
-('alice123', 'alice@example.com', 'hashed123', 'owner')
+('alice123', 'alice@example.com', 'hashed123', 'owner'),
+('bobwalker', 'bob@example.com', 'hashed123', 'owner'),
+('alice123', 'alice@example.com', 'hashed123', 'owner');
