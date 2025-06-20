aefc7b981a66fa64b71792d2093fc06c34443b71
ksor029 <unknown@student.adelaide.edu.au>
2025-06-20 13:03:47 +0000

    [automatic commit] 2025-06-20 13:03:46 UTC 49.181.211.143

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -6,16 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/* TEST API END POINTS */
-router.get('/api/dogs', function(req, res, next){
-  // code
-});
-router.get('/api/walkrequests/open', function(req, res, next){
-  // code
-});
-router.get('/api/walkers/summary', function(req, res, next){
-  // code
-});
-
-
 module.exports = router;
