4cdc687544c64a249bb92fe0ab2173ee24c7fdc5
ksor029 <unknown@student.adelaide.edu.au>
2025-06-20 10:57:48 +0000

    [automatic commit] 2025-06-20 10:57:48 UTC 60.242.86.238

--- part2/app.js
+++ part2/app.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
+var mysql = require('mysql');
+
+
+
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
