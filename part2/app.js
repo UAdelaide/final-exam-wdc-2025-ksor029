f3b624b1c29bf23d8efc11377c2bf358784fb332
ksor029 <unknown@student.adelaide.edu.au>
2025-06-20 11:08:09 +0000

    [automatic commit] 2025-06-20 11:08:09 UTC 60.242.86.238

--- part2/app.js
+++ part2/app.js
@@ -21,11 +21,11 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
 // Routes
-const walkRoutes = require('./routes/walkRoutes');
-const userRoutes = require('./routes/userRoutes');
+// const walkRoutes = require('./routes/walkRoutes');
+// const userRoutes = require('./routes/userRoutes');
 
-app.use('/api/walks', walkRoutes);
-app.use('/api/users', userRoutes);
+// app.use('/api/walks', walkRoutes);
+// app.use('/api/users', userRoutes);
 
 // Export the app instead of listening here
 module.exports = app;
