9c220d9f696f3a66c725562551379e78ba2bfe93
ksor029 <unknown@student.adelaide.edu.au>
2025-06-20 11:01:15 +0000

    [automatic commit] 2025-06-20 11:01:15 UTC 60.242.86.238

--- part2/app.js
+++ part2/app.js
@@ -2,18 +2,20 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config();
 
-const app = express();
-
-// Middleware
-app.use(express.json());
-app.use(express.static(path.join(__dirname, '/public')));
-
 var mysql = require('mysql');
 
 var dbConnectionPool = mysql.createPool({
     host: 'localhost',
     database: 'dogwalks'
 });
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '/public')));
+
+
 
 
 // Routes
